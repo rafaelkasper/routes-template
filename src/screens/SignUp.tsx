eeeea5464a1d8d13657e9e55683c2e73ebadbb1c
Rafael Kasper <rafael.kasper@cigam.com.br>
2024-02-03 19:38:59 -0300

    Estrutura inicial

---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,35 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { AuthNavigatorRoutesProps } from "../routes/auth.routes";
+
+export default function SignUp() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign up</Text>
+
+      <Text style={{ marginBottom: 12 }}>Esta tela usa stack navigation</Text>
+
+      <Button
+        title="Voltar para o Login"
+        onPress={() => navigation.navigate("signIn")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#dbe6fe",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 4,
+  },
+});
